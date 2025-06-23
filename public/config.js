f8cb40e4ac78ac4080a29bfd2ad1cf775315b9f0
Andrei Cristea <cristea.r.andrei@gmail.com>
2025-06-23 13:27:54 +0300

    added email to variables and changed for gh pages

---
+++ public/config.js
@@ -0,0 +1,8 @@
+// Runtime configuration for EmailJS
+// This file can be updated without rebuilding the application
+window.APP_CONFIG = {
+  EMAILJS_PUBLIC_KEY: 'your_public_key_here',
+  EMAILJS_SERVICE_ID: 'your_service_id_here', 
+  EMAILJS_TEMPLATE_ID: 'your_template_id_here',
+  NOTIFICATION_EMAIL: 'your_notification_email_here'
+}; 
